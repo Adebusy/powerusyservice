6517d6d5ceb79535ebe7dffb610816fb1dc1822c
Adebusy <alao.adebusy@gmail.com>
2022-03-31 10:31:01 +0100

    gcp update

--- app/driver/sqlserver.go
+++ app/driver/sqlserver.go
@@ -2,6 +2,7 @@ package driver
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/Adebusy/powerusyservice/utilities"
 	"github.com/jinzhu/gorm"
@@ -16,33 +17,46 @@ func init() {
 	godotenv.Load()
 	connectionString := fmt.Sprintf("sqlserver://%s:%s@localhost:1433?database=%s", utilities.GoDotEnvVariable("UserID"), utilities.GoDotEnvVariable("Password"), utilities.GoDotEnvVariable("Database"))
 	DbGorm, ErrGorm = gorm.Open("mssql", connectionString)
-	//DbGorm.Debug().DropTableIfExists()
-	//DbGorm.Debug().DropTableIfExists(&models.Tbl_users{})
-	//DbGorm.Debug().DropTableIfExists(&models.Tbl_Registered{})
-	//DbGorm.Debug().DropTableIfExists(&models.Tbl_ImportationDocument{})
-	//DbGorm.Debug().DropTableIfExists(&models.Tbl_auditlog{})
-	//DbGorm.Debug().DropTableIfExists(&models.Tbl_clearing{})
-	//DbGorm.Debug().DropTableIfExists(&models.Tbl_goodstype{})
-	//DbGorm.Debug().DropTableIfExists(&models.Tbl_importation{})
-	//DbGorm.Debug().DropTableIfExists(&models.Tbl_servicetype{})
-	//DbGorm.Debug().DropTableIfExists(&models.Tbl_subgoodstype{})
 
-	DbGorm.SingularTable(true)
-	//DbGorm.Debug().AutoMigrate(&models.Tbl_users{})
-	//DbGorm.Debug().AutoMigrate(&models.Tbl_Registered{})
-	//DbGorm.Debug().AutoMigrate(&models.Tbl_ImportationDocument{})
-	//DbGorm.Debug().AutoMigrate(&models.Tbl_KYC{})
-	//DbGorm.Debug().AutoMigrate(&models.Tbl_role{})
-	//DbGorm.Debug().AutoMigrate(&models.Tbl_status{})
+	os.Setenv("DBStatus", "dbcreateds")
+	if utilities.GoDotEnvVariable("DBStatus") == "true" {
+		//DbGorm.Debug().DropTableIfExists()
+		//DbGorm.Debug().DropTableIfExists(&models.Tbl_users{})
+		//DbGorm.Debug().DropTableIfExists(&models.Tbl_Registered{})
+		//DbGorm.Debug().DropTableIfExists(&models.Tbl_ImportationDocument{})
 
-	// DbGorm.Debug().AutoMigrate(&models.Tbl_auditlog{})
-	// DbGorm.Debug().AutoMigrate(&models.Tbl_clearing{})
-	// DbGorm.Debug().AutoMigrate(&models.Tbl_goodstype{})
-	// DbGorm.Debug().AutoMigrate(&models.Tbl_importation{})
-	// DbGorm.Debug().AutoMigrate(&models.Tbl_servicetype{})
-	// DbGorm.Debug().AutoMigrate(&models.Tbl_shipper{})
-	// DbGorm.Debug().AutoMigrate(&models.Tbl_subgoodstype{})
+		//DbGorm.Debug().DropTableIfExists(&models.Tbl_KYC{})
+		//DbGorm.Debug().DropTableIfExists(&models.Tbl_role{})
+		//DbGorm.Debug().DropTableIfExists(&models.Tbl_status{})
 
+		//DbGorm.Debug().DropTableIfExists(&models.Tbl_auditlog{})
+		//DbGorm.Debug().DropTableIfExists(&models.Tbl_clearing{})
+		//DbGorm.Debug().DropTableIfExists(&models.Tbl_goodstype{})
+		//DbGorm.Debug().DropTableIfExists(&models.Tbl_importation{})
+		//DbGorm.Debug().DropTableIfExists(&models.Tbl_servicetype{})
+		//DbGorm.Debug().DropTableIfExists(&models.Tbl_shipper{})
+		//DbGorm.Debug().DropTableIfExists(&models.Tbl_subgoodstype{})
+
+		DbGorm.SingularTable(true)
+		//DbGorm.Debug().AutoMigrate(&models.Tbl_users{})
+		//DbGorm.Debug().AutoMigrate(&models.Tbl_Registered{})
+		//DbGorm.Debug().AutoMigrate(&models.Tbl_ImportationDocument{})
+		//DbGorm.Debug().AutoMigrate(&models.Tbl_KYC{})
+		//DbGorm.Debug().AutoMigrate(&models.Tbl_role{})
+		//DbGorm.Debug().AutoMigrate(&models.Tbl_status{})
+
+		// DbGorm.Debug().AutoMigrate(&models.Tbl_auditlog{})
+		// DbGorm.Debug().AutoMigrate(&models.Tbl_clearing{})
+		// DbGorm.Debug().AutoMigrate(&models.Tbl_goodstype{})
+		// DbGorm.Debug().AutoMigrate(&models.Tbl_importation{})
+		// DbGorm.Debug().AutoMigrate(&models.Tbl_servicetype{})
+		// DbGorm.Debug().AutoMigrate(&models.Tbl_shipper{})
+		// DbGorm.Debug().AutoMigrate(&models.Tbl_subgoodstype{})
+	}
+
+	fmt.Println("03")
+	fmt.Println(os.Getenv("DBStatus"))
+	fmt.Println("04")
 	if ErrGorm != nil {
 		fmt.Printf(ErrGorm.Error())
 		return
